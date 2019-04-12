ea668a094ece457608902530c20e2c00ad1058d6
Werner de Groot <werner.de.groot@ordina.nl>
2019-04-12 19:38:41 +0200

    Naming in tests.

--- src/test/createAsyncSelector.test.ts
+++ src/test/createAsyncSelector.test.ts
@@ -24,38 +24,38 @@ describe('createAsyncSelector', () => {
 
     describe('producing a regular value', () => {
       it('should correctly combine two regular selectors', () => {
-        const res = createAsyncSelector(stringSelector, numberSelector, (s, n) => s.length + n)
+        const asyncSelector = createAsyncSelector(stringSelector, numberSelector, (s, n) => s.length + n)
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncValueReceived(5), [])
         expect(toVerify).toEqual(expected)
       })
 
       it('should run only once when called with the same arguments twice', () => {
         let numberOfTimesExecuted = 0
-        const res = createAsyncSelector(stringSelector, numberSelector, (s, n) => {
+        const asyncSelector = createAsyncSelector(stringSelector, numberSelector, (s, n) => {
           numberOfTimesExecuted += 1
           return s.length + n
         })
 
         // Call the selector twice, with the same arguments:
-        res({ ...initialAppState, version: 1 })
-        res({ ...initialAppState, version: 2 })
+        asyncSelector({ ...initialAppState, version: 1 })
+        asyncSelector({ ...initialAppState, version: 2 })
 
         expect(numberOfTimesExecuted).toEqual(1)
       })
 
       it('should run again when the arguments change', () => {
         let numberOfTimesExecuted = 0
-        const res = createAsyncSelector(stringSelector, numberSelector, (s, n) => {
+        const asyncSelector = createAsyncSelector(stringSelector, numberSelector, (s, n) => {
           numberOfTimesExecuted += 1
           return s.length + n
         })
 
         // Call the selector three times, with the different arguments:
-        res({ version: 1, num: 1, str: 'one' })
-        res({ version: 2, num: 1, str: 'uno' })
-        res({ version: 3, num: 3, str: 'uno' })
+        asyncSelector({ version: 1, num: 1, str: 'one' })
+        asyncSelector({ version: 2, num: 1, str: 'uno' })
+        asyncSelector({ version: 3, num: 3, str: 'uno' })
 
         expect(numberOfTimesExecuted).toEqual(3)
       })
@@ -63,25 +63,25 @@ describe('createAsyncSelector', () => {
 
     describe('producing a `AsyncValue`', () => {
       it('should correctly combine two regular selectors and return a `AsyncCommand` when the combinator returns that', () => {
-        const res = createAsyncSelector(stringSelector, numberSelector, (_s, _n) => asyncCommand<Command>([{ type: CommandType.DoSomething }]))
+        const asyncSelector = createAsyncSelector(stringSelector, numberSelector, (_s, _n) => asyncCommand<Command>([{ type: CommandType.DoSomething }]))
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncCommand([{ type: CommandType.DoSomething }]), [])
         expect(toVerify).toEqual(expected)
       })
 
       it('should correctly combine two regular selectors and return a `AsyncAwaitingValue` when the combinator returns that', () => {
-        const res = createAsyncSelector(stringSelector, numberSelector, (_s, _n) => asyncAwaitingValue())
+        const asyncSelector = createAsyncSelector(stringSelector, numberSelector, (_s, _n) => asyncAwaitingValue())
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncAwaitingValue(), [])
         expect(toVerify).toEqual(expected)
       })
 
       it('should correctly combine two regular selectors and return a `AsyncValueReceived` when the combinator returns that', () => {
-        const res = createAsyncSelector(stringSelector, numberSelector, (s, n) => asyncValueReceived(s.length + n))
+        const asyncSelector = createAsyncSelector(stringSelector, numberSelector, (s, n) => asyncValueReceived(s.length + n))
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncValueReceived(5), [])
         expect(toVerify).toEqual(expected)
       })
@@ -99,26 +99,26 @@ describe('createAsyncSelector', () => {
     describe('producing a regular value', () => {
       it('should return an `AsyncValueReceived` when all async selectors produce an `AsyncValueReceived`', () => {
         let wasCalled = false
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           wasCalled = true
           return s.length + n * (b ? 2 : 1)
         })
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncValueReceived(5), [])
         expect(toVerify).toEqual(expected)
         expect(wasCalled).toEqual(true)
       })
 
-      it('should return an `AsyncAwaitingValue` when one of the async selectors produces an `AsyncAwaitingValue` and the result produce an `AsyncValueReceived`', () => {
+      it('should return an `AsyncAwaitingValue` when one of the async selectors produces an `AsyncAwaitingValue` and the rest produces an `AsyncValueReceived`', () => {
         let wasCalled = false
         const appState: AppState = { version: 1, num: asyncAwaitingValue(), str: asyncValueReceived('one'), bool: false }
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           wasCalled = true
           return s.length + n * (b ? 2 : 1)
         })
 
-        const toVerify = res(appState)
+        const toVerify = asyncSelector(appState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncAwaitingValue(), [])
         expect(toVerify).toEqual(expected)
         expect(wasCalled).toEqual(false)
@@ -132,12 +132,12 @@ describe('createAsyncSelector', () => {
           str: asyncCommand([{ type: CommandType.DoSomething }, { type: CommandType.DoSomethingElse }]),
           bool: false
         }
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           wasCalled = true
           return s.length + n * (b ? 2 : 1)
         })
 
-        const toVerify = res(appState)
+        const toVerify = asyncSelector(appState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncCommand([{ type: CommandType.DoSomething }, { type: CommandType.DoSomethingElse }]), [])
         expect(toVerify).toEqual(expected)
         expect(wasCalled).toEqual(false)
@@ -151,12 +151,12 @@ describe('createAsyncSelector', () => {
           str: asyncCommand([{ type: CommandType.DoSomething }]),
           bool: false
         }
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           wasCalled = true
           return s.length + n * (b ? 2 : 1)
         })
 
-        const toVerify = res(appState)
+        const toVerify = asyncSelector(appState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncCommand([{ type: CommandType.DoSomething }, { type: CommandType.DoSomethingElse }]), [])
         expect(toVerify).toEqual(expected)
         expect(wasCalled).toEqual(false)
@@ -164,43 +164,43 @@ describe('createAsyncSelector', () => {
 
       it('should run only once when called with the same arguments twice', () => {
         let numberOfTimesExecuted = 0
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           numberOfTimesExecuted += 1
           return s.length + n * (b ? 2 : 1)
         })
 
         // Call the selector twice, with the same arguments:
-        res({ ...initialAppState, version: 1 })
-        res({ ...initialAppState, version: 2 })
+        asyncSelector({ ...initialAppState, version: 1 })
+        asyncSelector({ ...initialAppState, version: 2 })
 
         expect(numberOfTimesExecuted).toEqual(1)
       })
 
       it('should run only once when called with different async values, which contain the same values', () => {
         let numberOfTimesExecuted = 0
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           numberOfTimesExecuted += 1
           return s.length + n * (b ? 2 : 1)
         })
 
         // Call the selector twice, with different async values which contain the same values:
-        res({ ...initialAppState, version: 1, str: asyncValueReceived('one') })
-        res({ ...initialAppState, version: 2, str: asyncValueReceived('one') })
+        asyncSelector({ ...initialAppState, version: 1, str: asyncValueReceived('one') })
+        asyncSelector({ ...initialAppState, version: 2, str: asyncValueReceived('one') })
 
         expect(numberOfTimesExecuted).toEqual(1)
       })
 
       it('should run again when the arguments change', () => {
         let numberOfTimesExecuted = 0
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (s, n, b) => {
           numberOfTimesExecuted += 1
           return s.length + n * (b ? 2 : 1)
         })
 
         // Call the selector three times, with the different arguments:
-        res({ ...initialAppState, version: 1, str: asyncValueReceived('one'), num: asyncValueReceived(2) })
-        res({ ...initialAppState, version: 1, str: asyncValueReceived('uno'), num: asyncValueReceived(2) })
-        res({ ...initialAppState, version: 1, str: asyncValueReceived('uno'), num: asyncValueReceived(3) })
+        asyncSelector({ ...initialAppState, version: 1, str: asyncValueReceived('one'), num: asyncValueReceived(2) })
+        asyncSelector({ ...initialAppState, version: 1, str: asyncValueReceived('uno'), num: asyncValueReceived(2) })
+        asyncSelector({ ...initialAppState, version: 1, str: asyncValueReceived('uno'), num: asyncValueReceived(3) })
 
         expect(numberOfTimesExecuted).toEqual(3)
       })
@@ -208,17 +208,19 @@ describe('createAsyncSelector', () => {
 
     describe('producing a `AsyncValue`', () => {
       it('should correctly combine two async selectors and a regular selector and return a `AsyncCommand` when the combinator returns that', () => {
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (_s, _n, _b) => asyncCommand<Command>([{ type: CommandType.DoSomething }]))
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (_s, _n, _b) =>
+          asyncCommand<Command>([{ type: CommandType.DoSomething }])
+        )
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncCommand([{ type: CommandType.DoSomething }]), [])
         expect(toVerify).toEqual(expected)
       })
 
       it('should correctly combine two async selectors and a regular selector and return a `AsyncAwaitingValue` when the combinator returns that', () => {
-        const res = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (_s, _n, _b) => asyncAwaitingValue())
+        const asyncSelector = createAsyncSelector(asyncStringSelector, asyncNumberSelector, boolSelector, (_s, _n, _b) => asyncAwaitingValue())
 
-        const toVerify = res(initialAppState)
+        const toVerify = asyncSelector(initialAppState)
         const expected = asyncSelectorResult<AppState, Command, number>(asyncAwaitingValue(), [])
         expect(toVerify).toEqual(expected)
       })
